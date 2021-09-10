31352f3fdd4a42ef876c2b7089e4ffce2a6a6924
alepih <alexander.pihl@paf.com>
2021-09-10 14:36:43 +0300

    Support removing notes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::convert::Infallible;
+use std::str::FromStr;
 use std::sync::atomic::AtomicU64;
 use std::sync::Arc;
 
@@ -7,8 +8,24 @@ use tokio::sync::Mutex;
 use warp::http::StatusCode;
 use warp::{Filter, Reply};
 
+#[derive(Clone, Deserialize, Serialize, PartialEq)]
+struct NoteId(u64);
+
+impl FromStr for NoteId {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if let Ok(id) = u64::from_str(s) {
+            Ok(NoteId(id))
+        } else {
+            Err(())
+        }
+    }
+}
+
 #[derive(Serialize)]
 struct NoteResponse {
+    id: NoteId,
     title: String,
 }
 
@@ -22,13 +39,13 @@ struct CreateNoteRequest {
 
 #[derive(Clone)]
 struct Note {
-    id: u64,
+    id: NoteId,
     title: String,
 }
 
 type NoteDatabase = Arc<Mutex<Vec<Note>>>;
 
-static NEXT_NOTE_ID: AtomicU64 = AtomicU64::new(0);
+static NEXT_NOTE_ID: AtomicU64 = AtomicU64::new(1);
 
 async fn list_notes(db: NoteDatabase) -> Result<impl Reply, Infallible> {
     let db = db.lock().await;
@@ -36,6 +53,7 @@ async fn list_notes(db: NoteDatabase) -> Result<impl Reply, Infallible> {
     let notes = db
         .iter()
         .map(|note| NoteResponse {
+            id: note.id.clone(),
             title: note.title.to_owned(),
         })
         .collect();
@@ -45,16 +63,30 @@ async fn list_notes(db: NoteDatabase) -> Result<impl Reply, Infallible> {
 
 async fn create_note(db: NoteDatabase, req: CreateNoteRequest) -> Result<impl Reply, Infallible> {
     let new_note = Note {
-        id: NEXT_NOTE_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed),
+        id: NoteId(NEXT_NOTE_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed)),
         title: req.title,
     };
 
     let mut db = db.lock().await;
     db.push(new_note.clone());
 
-    Ok(warp::reply::json(&NoteResponse {
+    let body = warp::reply::json(&NoteResponse {
+        id: new_note.id,
         title: new_note.title,
-    }))
+    });
+    Ok(warp::reply::with_status(body, StatusCode::CREATED))
+}
+
+async fn remove_note(id: NoteId, db: NoteDatabase) -> Result<impl Reply, Infallible> {
+    let mut db = db.lock().await;
+    let old_len = db.len();
+    db.retain(|note| note.id != id);
+
+    if old_len != db.len() {
+        Ok(StatusCode::NO_CONTENT.into_response())
+    } else {
+        Ok(StatusCode::NOT_FOUND.into_response())
+    }
 }
 
 #[tokio::main]
@@ -68,16 +100,22 @@ async fn main() {
         .and(note_database_filter.clone())
         .and_then(list_notes);
 
-    let post_note_handler = warp::path!("notes")
+    let create_note_handler = warp::path!("notes")
         .and(warp::post())
-        .and(note_database_filter)
+        .and(note_database_filter.clone())
         .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
         .and_then(create_note);
+
+    let remove_note_handler = warp::path!("notes" / NoteId)
+        .and(warp::delete())
+        .and(note_database_filter)
+        .and_then(remove_note);
 
     let not_found_handler = warp::any().map(move || StatusCode::NOT_FOUND.into_response());
 
     let routes = list_notes_handler
-        .or(post_note_handler)
+        .or(create_note_handler)
+        .or(remove_note_handler)
         .or(not_found_handler);
 
     warp::serve(routes).run(([127, 0, 0, 1], 8080)).await;
