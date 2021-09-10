3f78c3316876ea4d4ffd80baa274d2e6b6da6897
alepih <alexander.pihl@paf.com>
2021-09-10 15:55:51 +0300

    Partially updating notes

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ impl FromStr for NoteId {
 struct NoteResponse {
     id: NoteId,
     title: String,
+    content: String,
 }
 
 #[derive(Serialize)]
@@ -35,12 +36,21 @@ struct NotesResponse(Vec<NoteResponse>);
 #[derive(Deserialize)]
 struct CreateNoteRequest {
     title: String,
+    #[serde(default)]
+    content: String,
+}
+
+#[derive(Deserialize)]
+struct UpdateNoteRequest {
+    title: Option<String>,
+    content: Option<String>,
 }
 
 #[derive(Clone)]
 struct Note {
     id: NoteId,
     title: String,
+    content: String,
 }
 
 type NoteDatabase = Arc<Mutex<Vec<Note>>>;
@@ -55,6 +65,7 @@ async fn list_notes(db: NoteDatabase) -> Result<impl Reply, Infallible> {
         .map(|note| NoteResponse {
             id: note.id.clone(),
             title: note.title.to_owned(),
+            content: note.content.to_owned(),
         })
         .collect();
 
@@ -65,6 +76,7 @@ async fn create_note(db: NoteDatabase, req: CreateNoteRequest) -> Result<impl Re
     let new_note = Note {
         id: NoteId(NEXT_NOTE_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed)),
         title: req.title,
+        content: req.content,
     };
 
     let mut db = db.lock().await;
@@ -73,8 +85,31 @@ async fn create_note(db: NoteDatabase, req: CreateNoteRequest) -> Result<impl Re
     let body = warp::reply::json(&NoteResponse {
         id: new_note.id,
         title: new_note.title,
+        content: new_note.content,
     });
     Ok(warp::reply::with_status(body, StatusCode::CREATED))
+}
+
+async fn update_note(id: NoteId, db: NoteDatabase, req: UpdateNoteRequest) -> Result<impl Reply, Infallible> {
+    if req.title.is_none() && req.content.is_none() {
+        return Ok(StatusCode::UNPROCESSABLE_ENTITY.into_response());
+    }
+
+    let mut db = db.lock().await;
+    let note_idx = db.iter().position(|note| note.id == id);
+    let note = match note_idx.and_then(|idx| db.get_mut(idx)) {
+        Some(note) => note,
+        None => return Ok(StatusCode::NOT_FOUND.into_response()),
+    };
+
+    if let Some(new_title) = req.title {
+        note.title = new_title;
+    }
+    if let Some(new_content) = req.content {
+        note.content = new_content;
+    }
+    
+    Ok(StatusCode::NO_CONTENT.into_response())
 }
 
 async fn remove_note(id: NoteId, db: NoteDatabase) -> Result<impl Reply, Infallible> {
@@ -106,6 +141,12 @@ async fn main() {
         .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
         .and_then(create_note);
 
+    let update_note_handler = warp::path!("notes" / NoteId)
+        .and(warp::patch())
+        .and(note_database_filter.clone())
+        .and(warp::body::content_length_limit(1024 * 16).and(warp::body::json()))
+        .and_then(update_note);
+
     let remove_note_handler = warp::path!("notes" / NoteId)
         .and(warp::delete())
         .and(note_database_filter)
@@ -115,6 +156,7 @@ async fn main() {
 
     let routes = list_notes_handler
         .or(create_note_handler)
+        .or(update_note_handler)
         .or(remove_note_handler)
         .or(not_found_handler);
 
